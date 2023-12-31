10d874af964ecc15de970eb3e57cb3929840cbc1
red2922 <inthavongsaj@lopers.unk.edu>
2023-12-31 13:12:35 -0600

    Added a validator and started script.js

---
+++ HTML/FreecodeCampProjects/PalindromeChecker/script.js
@@ -0,0 +1,9 @@
+textInput = document.getElementById("text-input");
+checkButton = document.getElementById("check-btn");
+output = document.getElementById("result");
+
+function isEmpty() {
+    if(textInput.value){
+        
+    }
+}
