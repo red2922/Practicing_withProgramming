d55bc79d5bbbc6f4a9f4a96fcb29042014b4babb
red2922 <inthavongsaj@lopers.unk.edu>
2024-01-01 20:18:34 -0600

    Steps 15 - 39

--- HTML/FreecodeCampProjects/FootballCards/script.js
+++ HTML/FreecodeCampProjects/FootballCards/script.js
@@ -189,3 +189,35 @@ const myFavoriteFootballTeam = {
 };
 
 Object.freeze(myFavoriteFootballTeam);
+
+//const sport = myFavoriteFootballTeam.sport;
+//const team = myFavoriteFootballTeam.team;
+
+//Same as the two above. Called Object Deconstructing
+const {sport, team, year, players} = myFavoriteFootballTeam;
+const {coachName} = myFavoriteFootballTeam.headCoach;
+
+typeOfSport.textContent = sport;
+teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
+
+//By setting a parameter equal to a variable it will default to that value when nothing is passed through
+const setPlayerCards = (arr = players) => {
+    playerCards.innerHTML += arr.map(({name,position,number,isCaptain,nickname} = arr) => 
+    {
+        `<div class="player-card">
+            <h2>${name}${isCaptain ? "(Captain)" : ""}</h2>
+            <p>Position: ${position}</p>
+            <p>Number: ${number}</p>
+            <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
+        </div>`;
+
+    }).join("");
+};
+
+playersDropdownList.addEventListener("change", (e) => {
+    playerCards.innerHTML = "";
+
+
+});
