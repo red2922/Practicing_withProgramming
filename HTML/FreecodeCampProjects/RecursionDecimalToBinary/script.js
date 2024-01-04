d42f55577b7982613e40aff17bb1a3c0e30d5613
red2922 <inthavongsaj@lopers.unk.edu>
2024-01-04 16:12:04 -0600

    Decimal to Binary Completed 45 - 109

--- HTML/FreecodeCampProjects/RecursionDecimalToBinary/script.js
+++ HTML/FreecodeCampProjects/RecursionDecimalToBinary/script.js
@@ -1,13 +1,73 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+const animationContainer = document.getElementById("animation-container");
+const animationData = [{
+    inputVal : 5,
+    marginTop: 300,
+    addElDelay : 1000,
+    msg : `decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.`,
+    showMsgDelay: 15000,
+    removeElDelay: 20000
+},
+
+{
+    inputVal : 2,
+    marginTop: -200,
+    addElDelay : 1500,
+    msg: `decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.`,
+    showMsgDelay: 10000,
+    removeElDelay: 15000
+},
+
+{
+    inputVal : 1,
+    marginTop: -200,
+    addElDelay : 2000,
+    msg: `decimalToBinary(1) returns "1" (base case) and gives that value to the stack below. Then it pops off the stack.`,
+    showMsgDelay: 5000,
+    removeElDelay: 10000
+}
+];
+
+//Messing with different datastructures
+//Stack LIFO. Grows down. With the top being the bottom and the bottom being the top.
+//A is the bottom/beginning of the stack while C is the top/start of the stack. You have to remove from the top first then you can remove A 
+const callStack = 
+['a(): returns "freeCodeCamp " + b()', 
+'b(): returns "is " + c()',
+'c(): returns "awesome!"'];
 
 const a = () => "freeCodeCamp " + b();
+const b = () => "is " + c();
+const c = () => "awesome!";
 
+//Actually Application functions
+//Recursive function calls itself over and over until base case is completed
+//Recursive function to understand it 
+const countDownAndUp = (number) => {
+
+    if(number === 0){ //Base case
+        return;
+
+    } else {
+        countDownAndUp(number - 1);
+    }
+
+};
 
 
 const decimalToBinary = (input) => {
+
+    if(input === 0 || input === 1){
+        return String(input);
+    } else {
+        return decimalToBinary(Math.floor(input / 2)) + (input % 2); //By using the addition it will concatenate the remainder to everything else
+    }
+
+
     //Not recursive but still infinitly better than the one under it 
+    /*
     let binary = "";
 
     if(input === 0){
@@ -22,7 +82,7 @@ const decimalToBinary = (input) => {
     }
 
     result.innerText = binary;
-
+    */
 
     /* Non-recursive approach
     const inputs = [];
@@ -53,18 +113,45 @@ const decimalToBinary = (input) => {
     */
 };
 
+const showAnimation = () => {
+    result.innerText = "Call Stack Animation";
+    animationData.forEach((obj) => {
+        setTimeout(() => {
+            animationContainer.innerHTML += 
+            `<p id="${obj.inputVal}" style="margin-top : ${obj.marginTop}px;" class="animation-frame">decimalToBinary(${obj.inputVal})</p>`;
+        }, obj.addElDelay);
 
+        setTimeout(() => {
+            document.getElementById(obj.inputVal).textContent = obj.msg;
+        }, obj.showMsgDelay);
 
+        setTimeout(() => {
+            document.getElementById(obj.inputVal).remove();
+        }, obj.removeElDelay);
 
+        setTimeout(() => {
+            result.textContent = decimalToBinary(5);
+        }, 20000);
+
+    });
+};
 
 
 const checkUserInput = () => {
-    if(!numberInput.value || isNaN(parseInt(numberInput.value))){
+    const inputInt = parseInt(numberInput.value);
+
+    if(!numberInput.value || isNaN(inputInt)){
         alert("Please provide a decimal number");
         return;
     }
 
-    decimalToBinary(parseInt(numberInput.value));
+    if(inputInt === 5){
+        showAnimation();
+        return;
+    }
+
+
+    result.textContent = decimalToBinary(inputInt);
     numberInput.value = "";
 };
 
