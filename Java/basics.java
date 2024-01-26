023b32f637ed58822f79284740ee681db53f7a9e
red2922 <inthavongsaj@lopers.unk.edu>
2024-01-25 21:29:28 -0600

    Messed with loops a little bit

---
+++ Java/basics.java
@@ -0,0 +1,9 @@
+
+class basics {
+    public static void main(String[] args) {
+        String[] names = { "Rose", "Red", "Zero", "Lily", "Eclipse", "Candi", "Sno", "Violet" };
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
