91b2a508b58438ec4b048f7540ddeb9ecb26a4c5
red2922 <jinthavongsa1@gmail.com>
2024-03-03 16:06:13 -0600

    Added comments so people understand

--- Python/Homework_4.py
+++ Python/Homework_4.py
@@ -10,20 +10,25 @@ women = {"a": ["z", "x", "y", "u", "v", "w"], "b": ["y", "z", "w", "x", "v", "u"
          "c": ["v", "x", "w", "y", "u", "z"], "d": ["w", "y", "u", "x", "z", "u"],
          "e": ["u", "v", "x", "w", "y", "z"], "f": ["u", "w", "x", "v", "z", "y"]}
 
+
 # Question 1
 # Lets return a list of sets to show the pairs
 def gale_shap(re, propose):
     matches = []
     initial = len(propose)
 
+    #Makes a copy of the dictionary (This took too long to figure out for some reason D: )
     c1 = copy.deepcopy(re)
     c2 = copy.deepcopy(propose)
 
     # Proposer will approach their reciever index[0]. If proposer == receiver[0] (highest) append to matches and remove key from all others.
     # else go to next proposer.
 
+    #Will go in a loop until length of matches is equal to number of keys
     while len(matches) != initial:
 
+    #Loops through all the keys in the proposing dictionary. If proposer is equal to the highest reciever then add to
+    #matches and remove the value from all lists
         for p in c2:
             if p == c1[c2[p][0]][0]:
                 matches.append((p, c2[p][0]))
@@ -35,11 +40,13 @@ def gale_shap(re, propose):
                 for i in c2:
                     if removed in c2[i]:
                         c2[i].remove(removed)
+            #Else it will then remove the front most value in propose since there is no match
             else:
                 c2[p].pop(0)
-
+    #Returns a list of sets sorted in lexical order
     return sorted(matches)
 
+
 # Question 2
-print(gale_shap(men, women))
-print(gale_shap(women, men))
+print(f" Women proposing to men: {gale_shap(men, women)}")
+print(f" Men proposing to women: {gale_shap(women, men)}")
