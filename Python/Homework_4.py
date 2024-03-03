ada4556e6b581eeedbb0df72a1a63a17480a2d28
red2922 <jinthavongsa1@gmail.com>
2024-03-03 16:00:05 -0600

    Made code work as intended

--- Python/Homework_4.py
+++ Python/Homework_4.py
@@ -2,20 +2,11 @@
 # 3.2.4 Determine the stable matching resulting from the Proposal Algo run with men proposing and with women proposing given the list below
 import copy
 
-
 men = {"u": ["a", "b", "d", "c", "f", "e"], "v": ["a", "b", "c", "f", "e", "d"],
        "w": ["c", "b", "d", "a", "f", "e"], "x": ["c", "a", "d", "b", "e", "f"],
        "y": ["c", "d", "a", "b", "f", "e"], "z": ["d", "e", "f", "c", "b", "a"]}
 
 women = {"a": ["z", "x", "y", "u", "v", "w"], "b": ["y", "z", "w", "x", "v", "u"],
-         "c": ["v", "x", "w", "y", "u", "z"], "d": ["w", "y", "u", "x", "z", "u"],
-         "e": ["u", "v", "x", "w", "y", "z"], "f": ["u", "w", "x", "v", "z", "y"]}
-
-m1 = {"u": ["a", "b", "d", "c", "f", "e"], "v": ["a", "b", "c", "f", "e", "d"],
-       "w": ["c", "b", "d", "a", "f", "e"], "x": ["c", "a", "d", "b", "e", "f"],
-       "y": ["c", "d", "a", "b", "f", "e"], "z": ["d", "e", "f", "c", "b", "a"]}
-
-w1 = {"a": ["z", "x", "y", "u", "v", "w"], "b": ["y", "z", "w", "x", "v", "u"],
          "c": ["v", "x", "w", "y", "u", "z"], "d": ["w", "y", "u", "x", "z", "u"],
          "e": ["u", "v", "x", "w", "y", "z"], "f": ["u", "w", "x", "v", "z", "y"]}
 
@@ -25,28 +16,30 @@ def gale_shap(re, propose):
     matches = []
     initial = len(propose)
 
-    #Proposer will approach their reciever index[0]. If proposer == receiver[0] (highest) append to matches and remove key from all others.
+    c1 = copy.deepcopy(re)
+    c2 = copy.deepcopy(propose)
+
+    # Proposer will approach their reciever index[0]. If proposer == receiver[0] (highest) append to matches and remove key from all others.
     # else go to next proposer.
 
     while len(matches) != initial:
 
-        for p in propose:
-            if p == re[propose[p][0]][0]:
-                matches.append((p, propose[p][0]))
-                removed = propose[p][0]
-                for r in re:
-                    if p in re[r]:
-                        re[r].remove(p)
+        for p in c2:
+            if p == c1[c2[p][0]][0]:
+                matches.append((p, c2[p][0]))
+                removed = c2[p][0]
+                for r in c1:
+                    if p in c1[r]:
+                        c1[r].remove(p)
 
-                for i in propose:
-                    if removed in propose[i]:
-                        propose[i].remove(removed)
+                for i in c2:
+                    if removed in c2[i]:
+                        c2[i].remove(removed)
             else:
-                propose[p].pop(0)
+                c2[p].pop(0)
 
     return sorted(matches)
 
 # Question 2
-
 print(gale_shap(men, women))
-print(gale_shap(w1, m1))
+print(gale_shap(women, men))
