4cac7c16b84bf4f4eb254352868b7970500daf30
red2922 <inthavongsaj@lopers.unk.edu>
2024-04-02 22:39:53 -0500

    Started my Java 2.0 Journey

---
+++ Java/exerciseBasics.java
@@ -0,0 +1,19 @@
+public class exerciseBasics {
+    static void exer_1() {
+        System.out.println("Hello World");
+        System.out.println("Jake Inthavongsa");
+    }
+
+    static void exer_2() {
+        System.out.println(74 + 36);
+    }
+
+    static void exer_3() {
+
+    }
+
+    public static void main(String[] args) {
+        exer_1();
+        exer_2();
+    }
+}
