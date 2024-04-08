f3beaeffd8442e8e12314cbcf8514b00b572a1fa
red2922 <jinthavongsa1@gmail.com>
2024-04-08 18:58:45 -0500

    SMH

--- Python/Homework_5.py
+++ Python/Homework_5.py
@@ -1,25 +1,36 @@
-from collections import defaultdict
+import math
  
-# This class represents an directed graph
-# using adjacency list representation
- # class Graph is taken from GeeksForGeeks 
-#https://www.geeksforgeeks.org/tarjan-algorithm-find-strongly-connected-components/
- 
+class Node:
+    def __init__(self,u):
+       self.u = u
+       self.v = []
+       self.low = math.inf
+       self.disc = 0
+       self.parent = None
+       
+    def add_Edge(self, e):
+        self.v.append(e)
+
 class Graph:
  
     def __init__(self, vertices):
-        # No. of vertices
         self.V = vertices
- 
-        # default dictionary to store graph
-        self.graph = defaultdict(list)
- 
-        self.Time = 0
- 
+        self.graph = []
+        for i in range(vertices):
+            self.graph.append([])
+        
     # function to add an edge to graph
     def addEdge(self, u, v):
         self.graph[u].append(v)
 
+
     #All code under this comment is made by me 
         
-    
+new = Graph(8)
+edges = [[1,2],[2,3],[3,4],[4,1],[2,4],[3,5],[5,6],[5,7],[5,8]]
+
+for i in edges:
+    new.addEdge(i[0] - 1,i[1])
+    new.addEdge(i[1] - 1, i[0])
+
+print(new.graph)
