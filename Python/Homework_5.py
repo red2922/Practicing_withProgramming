aa0cdd0fea57d114dd4798b787422e2511479a24
red2922 <jinthavongsa1@gmail.com>
2024-04-10 19:44:35 -0500

    Still not working

--- Python/Homework_5.py
+++ Python/Homework_5.py
@@ -23,23 +23,29 @@ class Graph:
     def addEdge(self, u, v):
         self.graph[u].add_Edge(v)
 
-def dfs(g ,v, stack):
+def dfs(g ,v, stack, time):
     current = g.graph[v - 1]
     current.disc = time
     current.low = time
 
     for i in current.v:
-        if i.disc == -1:
+        if g.graph[i - 1].disc == -1:
             stack.append([i,current.u])
             g.graph[i - 1].parent = i
 
-            dfs(g,v, stack)
+            g.graph[i - 1].disc = time
+            time += 1
+            dfs(g,v, stack, time)
+            g.graph[i - 1].low = min(current.low, g.graph[i - 1].low)
 
-            current.low = min(current.low, i.low)
-            if current.low >= i.disc:
+            
+            if current.low >= g.graph[i - 1].disc:
                 popped = None
-                while popped != [i,current.u]:
+                while popped != [i,current.u] and len(stack) != 0:
                     bioconnected.append(stack.pop())
+
+        if current.u != g.graph[i - 1].parent:
+            g.graph[i - 1].low = min(g.graph[i - 1].low, current.u)
                 
 
 new = Graph(8)
@@ -56,5 +62,20 @@ for i in edges:
 stack = []
 bioconnected = []
 time = 0
+
+#dfs(new, 1, stack, time)
+
+dfs(new, 1, stack, time)
+dfs(new, 2, stack, time)
+dfs(new, 3, stack, time)
+dfs(new, 4, stack, time)
+dfs(new, 5, stack, time)
+dfs(new, 6, stack, time)
+dfs(new, 7, stack, time)
+dfs(new, 8, stack, time)
+
+
+print(bioconnected)
+
 
 
